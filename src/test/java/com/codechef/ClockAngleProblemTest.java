29e8cb62e88a9cfc62c0ae5ac81e06d66a786d9d
Junie Bonifacio <juniebonifacio@Junies-MacBook-Air.local>
2017-03-01 20:15:44 +0800

    first commit

---
+++ src/test/java/com/codechef/ClockAngleProblemTest.java
@@ -0,0 +1,21 @@
+package com.codechef;
+
+import org.junit.Test;
+
+public class ClockAngleProblemTest {
+
+	@Test
+	public void test() {
+		int t = 3;
+		int hh = 12;
+		int mm = 00;
+		
+	}
+
+	public int findClockAngle(int t, int hh, int mm) {
+		
+		
+		
+		return 0;
+	}
+}
---
+++ src/test/java/com/codechef/NothingInCommonTest.java
@@ -0,0 +1,110 @@
+package com.codechef;
+
+import java.util.Scanner;
+
+import org.junit.Test;
+
+public class NothingInCommonTest {
+
+	/**
+	 * https://www.codechef.com/problems/NOTINCOM
+	 * 
+	 * Alice has quarreled with Berta recently. Now she doesn't want to have
+	 * anything in common with her! Recently, they've received two collections
+	 * of positive integers. The Alice's integers were A1, A2, ..., AN, while
+	 * Berta's were B1, B2, ..., BM. Now Alice wants to throw away the minimal
+	 * amount of number from her collection so that their collections would have
+	 * no common numbers, i.e. there won't be any number which is present in
+	 * both collections. Please help Alice to find the minimal amount of numbers
+	 * she would need to throw away.
+	 * 
+	 * Input The first line of the input contains an integer T denoting the
+	 * number of test cases. The description of T test cases follows. The first
+	 * line of each test case contains two space-separated integer numbers N and
+	 * M denoting the amount of numbers in Alice's and Berta's collections
+	 * respectively. The second line contains N space-separated integers A1, A2,
+	 * ..., AN denoting the numbers of Alice. The third line contains M
+	 * space-separated integers B1, B2, ..., BM denoting the numbers of Berta.
+	 * 
+	 * Output For each test case, output a single line containing the minimal
+	 * amount of numbers Alice needs to throw away from her collection so that
+	 * she wouldn't have any numbers in common with Berta.
+	 * 
+	 * Constraints 1 ≤ Ai, Bi ≤ 10^6 All numbers are distinct within a single
+	 * girl's collection.
+	 * 
+	 * Subtasks Subtask #1 (47 points): T = 25, 1 ≤ N, M ≤ 1000 Subtask #2 (53
+	 * points): T = 5, 1 ≤ N, M ≤ 100000
+	 * 
+	 * Example Input: 2 3 4 1 2 3 3 4 5 6 3 3 1 2 3 4 5 6
+	 * 
+	 * Output: 1 0
+	 * 
+	 * Explanation Example case 1. If Alice throws away the number 3 from her
+	 * collection, she would obtain {1, 2} which is disjoint with {3, 4, 5, 6}.
+	 * 
+	 * Example case 2. Girls don't have any number in common initially. So there
+	 * is no need to throw away any number.
+	 * 
+	 * 
+	 * Editorial: https://discuss.codechef.com/questions/90470/notincom-editorial
+	 * 
+	 * Solution: https://www.codechef.com/viewsolution/12669539
+	 * 			 https://www.codechef.com/viewsolution/12670397
+	 * 
+	 */
+
+	@Test
+	public void testNothingInCommon() {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		long count = 0;
+		while (t > 0) {
+			int A[] = new int[1000001];
+			int n = in.nextInt();
+			int m = in.nextInt();
+			for (int i = 0; i < n; i++) {
+				int a = in.nextInt();
+				A[a]++;
+			}
+
+			for (int i = 0; i < m; i++) {
+				int a = in.nextInt();
+				A[a]++;
+				if (A[a] == 2) {
+					count++;
+				}
+			}
+			System.out.println(count);
+			count = 0;
+			t--;
+		}
+	}
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int t = in.nextInt();
+		long count = 0;
+		while (t > 0) {
+			int A[] = new int[1000001];
+			int n = in.nextInt();
+			int m = in.nextInt();
+			for (int i = 0; i < n; i++) {
+				int a = in.nextInt();
+				A[a]++;
+			}
+
+			for (int i = 0; i < m; i++) {
+				int a = in.nextInt();
+				A[a]++;
+				if (A[a] == 2) {
+					count++;
+				}
+			}
+			System.out.println(count);
+			count = 0;
+			t--;
+		}
+	}
+
+}
